2c69f54a7750df7dce12995d83638e432ee6c121
Nadeer-Ansari <145560887+Nadeer-Ansari@users.noreply.github.com>
2025-04-27 09:46:22 +0000

    Update HPC1A.cpp

--- HPC1A.cpp
+++ HPC1A.cpp
@@ -6,128 +6,127 @@ using namespace std;
 
 class node
 {
-	public:
-	node *left, *right;
-	int data;
+public:
+    node *left, *right;
+    int data;
 };
 
 class Breadthfs
 {
-	public:
-	node *insert(node *, int);
-	void bfs(node *);
+public:
+    node *insert(node *, int);
+    void bfs(node *);
 };
 
-node *insert(node *root, int data)
-// inserts a node in tree
+node *Breadthfs::insert(node *root, int data)
 {
-	if (!root)
-	{
-		root = new node;
-		root->left = NULL;
-		root->right = NULL;
-		root->data = data;
-		return root;
-	}
+    // inserts a node in tree
+    if (!root)
+    {
+        root = new node;
+        root->left = NULL;
+        root->right = NULL;
+        root->data = data;
+        return root;
+    }
 
-	queue<node *> q;
-	q.push(root);
+    queue<node *> q;
+    q.push(root);
 
-	while (!q.empty())
-	{
-		node *temp = q.front();
-		q.pop();
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
 
-		if (temp->left == NULL)
-		{
-			temp->left = new node;
-			temp->left->left = NULL;
-			temp->left->right = NULL;
-			temp->left->data = data;
-			return root;
-		}
+        if (temp->left == NULL)
+        {
+            temp->left = new node;
+            temp->left->left = NULL;
+            temp->left->right = NULL;
+            temp->left->data = data;
+            return root;
+        }
+        else
+        {
+            q.push(temp->left);
+        }
 
-		else
-		{
-			q.push(temp->left);
-		}
+        if (temp->right == NULL)
+        {
+            temp->right = new node;
+            temp->right->left = NULL;
+            temp->right->right = NULL;
+            temp->right->data = data;
+            return root;
+        }
+        else
+        {
+            q.push(temp->right);
+        }
+    }
 
-		if (temp->right == NULL)
-		{
-			temp->right = new node;
-			temp->right->left = NULL;
-			temp->right->right = NULL;
-			temp->right->data = data;
-			return root;
-		}
-
-		else
-		{
-			q.push(temp->right);
-		}
-	}
+    return root; // Ensure all paths return root
 }
 
-void bfs(node *head)
+void Breadthfs::bfs(node *head)
 {
-	queue<node *> q;
-	q.push(head);
+    queue<node *> q;
+    q.push(head);
 
-	int qSize;
+    int qSize;
 
-	while (!q.empty())
-	{
-		qSize = q.size();
+    while (!q.empty())
+    {
+        qSize = q.size();
 
-		#pragma omp parallel for
-		// creates parallel threads
-		for (int i = 0; i < qSize; i++)
-		{
-			node *currNode;
+        #pragma omp parallel for
+        // creates parallel threads
+        for (int i = 0; i < qSize; i++)
+        {
+            node *currNode;
 
-			#pragma omp critical
-			{
-				currNode = q.front();
-				q.pop();
-				cout << "\t" << currNode->data;
+            #pragma omp critical
+            {
+                currNode = q.front();
+                q.pop();
+                cout << "\t" << currNode->data;
+            } // prints parent node
 
-			} // prints parent node
-
-			#pragma omp critical
-			{
-				if (currNode->left) // push parent's left node in queue
-					q.push(currNode->left);
-				if (currNode->right)
-					q.push(currNode->right);
-			} // push parent's right node in queue
-		}
-	}
+            #pragma omp critical
+            {
+                if (currNode->left) // push parent's left node in queue
+                    q.push(currNode->left);
+                if (currNode->right) // push parent's right node in queue
+                    q.push(currNode->right);
+            }
+        }
+    }
 }
 
 int main()
 {
+    Breadthfs b; // Create an object of Breadthfs class
+    node *root = NULL;
+    int data;
+    char ans;
 
-	node *root = NULL;
-	int data;
-	char ans;
+    do
+    {
+        cout << "\nEnter data => ";
+        cin >> data;
 
-	do
-	{
-		cout << "\n enter data=>";
-		cin >> data;
+        root = b.insert(root, data); // Use class method to insert
 
-		root = insert(root, data);
+        cout << "Do you want to insert one more node? (y/n): ";
+        cin >> ans;
 
-		cout << "do you want insert one more node?";
-		cin >> ans;
+    } while (ans == 'y' || ans == 'Y');
 
-	} while (ans == 'y' || ans == 'Y');
+    double start = omp_get_wtime();
+    b.bfs(root); // Use class method for BFS
+    double end = omp_get_wtime();
 
-	double start = omp_get_wtime();
-	bfs(root);
-	double end = omp_get_wtime();
+    cout << "\nExecution Time: " << (end - start) << " seconds\n";
 
-	cout << "\nExecution Time: " << (end - start) << " seconds\n";
-
-	return 0;
+    return 0;
 }
